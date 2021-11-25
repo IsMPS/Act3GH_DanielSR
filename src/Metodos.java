538a951fce9aeb45ac0ac6fae581ff482fd57c7c
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2021-11-25 13:57:48 +0100

    Se ha creado un metodo para contar las repeticiones dentro del array

--- src/Metodos.java
+++ src/Metodos.java
@@ -2,15 +2,30 @@
 public class Metodos {
 	public void rellenarArray() {
 		int[] array = new int[20];
-		
+
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) Math.random() * 11;
 		}
 	}
-	
+
 	public void imprimirArray(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
+	}
+
+	public int[] contarRepeticiones(int[] array) {
+		int[] repeticiones = new int[array.length];
+
+		for (int i = 0; i < array.length; i++) {
+			repeticiones[i] = 0;
+			for (int j = 0; j < array.length; j++) {
+				if (array[i] == array[j]) {
+					repeticiones[i] += 1;
+				}
+			}
+		}
+		
+		return repeticiones;
 	}
 }
